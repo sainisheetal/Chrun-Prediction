edf2e793a2db73fa99753bd11210d0cdea482c8b
andygilet <andy-gilet@live.be>
2023-02-23 15:32:04 +0100

    change on the website

--- DEPLOYEMENT/api.py
+++ DEPLOYEMENT/api.py
@@ -2,19 +2,34 @@ from flask import Flask, render_template, request
 from flask_restful import Api
 from ressources.forms import user_entry_form
 from ressources.rest_class import User_request
+from data_traduction.traduction_columns import Gender_str_to_number, Education_Level_str_to_number, Marital_Status_str_to_number, Income_Category_str_to_number
 import pickle
+import pandas as pd
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/assets')
 app.config['SECRET_KEY'] = 'ValeureuxLiegeois'
 api = Api(app)
+
+gender_dictionnary = Gender_str_to_number()
+education_level_dictionnary = Education_Level_str_to_number()
+marital_status_dictionnary = Marital_Status_str_to_number()
+income_category_dictionnary = Income_Category_str_to_number()
+
+with open("model/model.pickle", "rb") as file:
+    model = pickle.load(file)
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     form = user_entry_form()
     if form.is_submitted():
         result = request.form
-        result = "model not implemented !!!"
-        return render_template('index.html', form=form, result=result)
+        data = [int(result["age"]), 
+                gender_dictionnary[result["gender"]], 
+                education_level_dictionnary[result["education_level"]], 
+                marital_status_dictionnary[result["marital_status"]], 
+                income_category_dictionnary[result["income_category"]]]
+        #result = model.predict(data)
+        return render_template('index.html', form=form, result=data)
     return render_template('index.html', form=form)
             
 api.add_resource(User_request, "/User")
--- DEPLOYEMENT/ressources/forms.py
+++ DEPLOYEMENT/ressources/forms.py
@@ -3,12 +3,9 @@ from wtforms import IntegerField, SelectField, SubmitField, FloatField
 
 class user_entry_form(FlaskForm):
     client_number = IntegerField('client_number')
-    attrition_flag = SelectField('attrition_flag', choices=["Existing Customer",
-                                                            "Attrited Customer"]) #Remettre les valeurs des types de propriété
     age = IntegerField('age')
     gender = SelectField('gender', choices=["M",
                                             "F"])
-    dependent_count = IntegerField('dependent_count')    
     education_level = SelectField('education_level', choices=["Unknown",
                                                               "Uneducated",
                                                               "High School",
@@ -26,23 +23,4 @@ class user_entry_form(FlaskForm):
                                                               "$60K - $80K",
                                                               "$80K - $120K",
                                                               "$120K +"])
-    
-    card_category = SelectField('card_category', choices=["Blue",
-                                                          "Silver",
-                                                          "Gold",
-                                                          "Platinum"])
-    months_on_book = IntegerField('months_on_book')
-    total_relationship_count = IntegerField('total_relationship_count')
-    months_inactive = IntegerField('months_inactive')
-    contacts_count = IntegerField('contacts_count')
-    credit_limit = FloatField('credit_limit')
-    total_revolving_bal = IntegerField('total_revolving_bal')
-    avg_open_to_buy = FloatField('avg_open_to_buy')
-    total_amt_chng = FloatField('total_amt_chng')
-    total_trans_amt = IntegerField('total_trans_amt')
-    total_trans_ct = IntegerField('total_trans_ct')
-    total_ct_chng = FloatField('total_ct_chng')
-    avg_utilization_ratio = FloatField('avg_utilization_ratio')
-    NBCFC3CDELMI1 = FloatField('NBCFC3CDELMI1')
-    NBCFC3CDELMI2 = FloatField('NBCFC3CDELMI2')
     submit = SubmitField('submit')
